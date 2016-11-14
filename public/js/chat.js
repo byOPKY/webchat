983a1278b0c112e6062bfed3b56e6c4300a3fef9
jenniferjara <lisbet.jara@gmail.com>
2016-11-14 18:53:34 -0500

    cmabio en chat.css

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,42 +1,39 @@
-jQuery(function($) {
-   var socket = io.connect();
-   var $messageForm = $('#send-message');
-   var $messageBox = $('#message');
-   var $chat = $('#chat');
-   var $buttonSend = $('#send');
-   
-   var $nickForm = $('#setNick');
-   var $nickBox = $('#nickname');
-   var $users = $('#users');
-   var $closeAlert = $('#closeAlert');  
-   
-   $nickForm.click(function(e) {
-       e.preventDefault();
-       socket.emit('new user', $nickBox.val(), function(data) {
-           if(data) {
-               $('#nickWrap').hide();
-               $('#contentWrap').show();
-           } else {
-               $("#login-error").show();
-           }
-       });
-       $nickBox.val('');
+var socket = io.connect();
+var $messageForm = $('#send-message');
+var $messageBox = $('#message');
+var $chat = $('#chat');
+var $buttonSend = $('#send');
+
+var $nickForm = $('#setNick');
+var $nickBox = $('#nickname');
+var $users = $('#users');
+var $closeAlert = $('#closeAlert');
+
+var chat = function(e) {
+   e.preventDefault();
+   socket.emit('new user', $nickBox.val(), function(data) {
+       if(data) {
+           $('#nickWrap').hide();
+           $('#contentWrap').show();
+       } else {
+           $("#login-error").show();
+       }
    });
-   
+   $nickBox.val('');
+}
+var iniciar = function($) {
+   $nickForm.click(chat);
    $closeAlert.click(function(e) {
         $("#login-error").hide();
    });
-   
    $messageForm.submit(function(e) {
        e.preventDefault();
        if($messageBox.val()!='') socket.emit('send message', $messageBox.val());
        $messageBox.val('');
    });
-   
    socket.on('new message', function(data) {
       $chat.append('<b>'+data.nick+":</b> "+data.msg+"<br/>"); 
    });
-   
    socket.on('usernames', function(data) {
         var html = '';
         for (var username in data) {
@@ -45,4 +42,5 @@ jQuery(function($) {
         $users.html(html);
     });
    
-});
+};
+jQuery(iniciar);
